87256562c13d743e7ee78126a5cfbd1305aacb15
Eriko Taniguchi <eriko.taniguchi@ntt.eu>
2017-08-31 11:21:30 +0100

    backbone-5

--- webapp/app.js
+++ webapp/app.js
@@ -11,16 +11,39 @@ var accountsCollection = new AccountCollection();
 accountsCollection.fetch({
     success: function(collection) {
         console.log('loaded accounts ', collection);
+        var accountsView = new AccountsView({
+            collection: accountsCollection
+        });
+        $('#js-app').html(accountsView.render().el)
     },
     error: function() {
         console.log('error loading accounts ', arguments);
+    }
+});
+
+var AccountItemView = Backbone.View.extend({
+    template: _.template($('#account-item-tmpl').html()),
+    tagName: 'li',
+    className: 'list-group-item',
+    render: function() {
+      var $el = $(this.el);
+      $el.html(this.template(this.model.toJSON()));
+      return this;
     }
 })
 
 var AccountsView = Backbone.View.extend({
     template: _.template($('#accounts-tmpl').html()),
+    renderListItem: function(model) {
+        var item = new AccountItemView({model: model});
+        $('.js-accounts-list', this.$el).append(item.render().el);
+    },
     render: function() {
+        var self = this;
         this.$el.html(this.template());
+        this.collection.each(function(account) {
+                self.renderListItem(account);
+        });
         return this;
     }
 });
